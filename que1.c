34cdc867658580d3b8f7a4917c968e616dd149e4
singh2034 <singhaayush2034@gmail.com>
2025-12-30 12:01:07 +0530

    question1-solved

---
+++ que1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Declare 3 integer variables a,b, and c assign value 50 to all three using a single statement
+
+int main() {
+    int a,b,c = 50;
+    printf("Here is the value of a,b,c %d \n", c);
+
+    return 0;
+}
