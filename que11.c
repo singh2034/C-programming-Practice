0741e39b2ae20b1dd80afa6a455721bc475df42d
singh2034 <singhaayush2034@gmail.com>
2026-01-08 15:28:50 +0530

    que11-solution

---
+++ que11.c
@@ -0,0 +1,16 @@
+// write a program to print - sum of two integers, sum of one integer and one float, sum of two floats.
+#include <stdio.h>
+
+
+int main () {
+    int a,b; float c,d;
+    int result;
+    // sum of 2 integers
+    a = 2; b = 3;
+    result = a+b;
+    printf("%d\n",  result);
+
+    
+    return 0;
+
+}
