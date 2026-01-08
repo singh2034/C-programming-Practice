f5cc89a5602b8d56df8bbac491638d3b68e5b4c2
singh2034 <singhaayush2034@gmail.com>
2026-01-08 15:43:39 +0530

    float error

--- que11.c
+++ que11.c
@@ -4,11 +4,16 @@
 
 int main () {
     int a,b; float c,d;
-    int result;
+    int result, result_2;
     // sum of 2 integers
     a = 2; b = 3;
     result = a+b;
     printf("%d\n",  result);
+
+    // sum of 1  float & 1 integer
+    c = 2.2; d = 4.2;
+    result_2 = c+d;
+    printf("%d\n", result_2);
 
     
     return 0;
