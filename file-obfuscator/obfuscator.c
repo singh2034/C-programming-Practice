731604bf3e3022e29f4576f6dfa7c3c9f293c9eb
singh2034 <singhaayush2034@gmail.com>
2026-01-28 11:33:32 +0530

    instructions added

--- file-obfuscator/obfuscator.c
+++ file-obfuscator/obfuscator.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
+
+// defines a function and takes an integer c - representing a character as input.
 int transform(int c) {
+	// using bitwise NOT Operator ~ for flipping the binary 0 to 1 and 1 to 0.
+	// for example :- for A -> in 8 bit binary is -> 01000001 and ~ using this -> 10111110 (flipped)
 	return ~c;
 }
 
 
 int main () {
+	// var declare so that, it stores a character being processed, for handling the EOF (End of file) signal.
+	// I have used int rather than char, so that it can handle the EOF signal very well.
 	int c;
-	while ((c =getchar()) != EOF)
+	// getchar() reads a single keyboard characters, and stores in the int c.
+	while ((c = getchar()) != EOF)
+		// putchar() is printing the transformed c character on the screen.
 		putchar(transform(c));
 }
