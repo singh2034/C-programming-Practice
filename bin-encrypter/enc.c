3ff42782f29d3cefebcba914dbf789237c534ad5
singh2034 <singhaayush2034@gmail.com>
2026-02-04 10:33:10 +0530

    encrypter-project :- Completed

--- bin-encrypter/enc.c
+++ bin-encrypter/enc.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int get_bitmask(char)
+int get_bitmask(char *key)
+{
+    int hash = 0;
+    while (*key)
+        hash += (*key++);
+    return hash;
+}
 
-int encrypt (int c, char* key) {
+int encrypt(int c, char *key)
+{
     int bitmask = get_bitmask(key);
     return c ^ bitmask;
 }
@@ -12,5 +19,5 @@ int main(int arc, char *argv[])
     int c;
     while ((c = getchar()) != EOF)
 
-        putchar(encrypt(c, argc[1]));
+        putchar(encrypt(c, argv[1]));
 }
