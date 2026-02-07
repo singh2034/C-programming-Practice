883b0e722aa5d5f6d94e6567f7c9421f17bded22
singh2034 <singhaayush2034@gmail.com>
2026-02-07 17:11:49 +0530

    progress

--- rotating-cube/cube.c
+++ rotating-cube/cube.c
@@ -29,14 +29,13 @@ int draw_point_3d(SDL_Surface *surface, struct Point point)
     draw_point(surface, x_2d, y_2d);
 }
 
-int draw_points_3d(SDL_Surface *surface, struct Point point[])
+int draw_points_3d(SDL_Surface *surface, struct Point points[], int number_of_points)
 {
-    // sizeof(point) / sizeof(point[0]);
-    int array_length = 100;
-    for (int i = 0; i < array_length; i++)
+
+    for (int i = 0; i < number_of_points; i++)
     {
-        int x_2d = point[i].x + COORDINATE_SYSTEM_OFFSET_X;
-        int y_2d = point[i].y + COORDINATE_SYSTEM_OFFSET_Y;
+        int x_2d = points[i].x + COORDINATE_SYSTEM_OFFSET_X;
+        int y_2d = points[i].y + COORDINATE_SYSTEM_OFFSET_Y;
         draw_point(surface, x_2d, y_2d);
     }
 }
@@ -46,12 +45,31 @@ void initialize_cube(struct Point points[], int number_of_points)
     // int number_of_points = 100;
     // struct Point points[number_of_points];
 
-    for (int i = 0; i < number_of_points; i++)
+    // cube has 12 sides
+    int points_per_side = number_of_points / 12;
+
+    int SIDE_LENGTH = 50;
+    int step_size = SIDE_LENGTH / points_per_side;
+    // side 1
+    for (int i = 0; i < points_per_side; i++)
     {
-        points[i] = (struct Point){i,
-                                   i,
-                                   i};
+        points[i] = (struct Point){-SIDE_LENGTH / 2 + i * step_size,
+                                   -SIDE_LENGTH / 2, +SIDE_LENGTH / 2};
     }
+
+    // side 2
+    for (int i = points_per_side; i < 2 * points_per_side; i++)
+    {
+        points[i] = (struct Point){-SIDE_LENGTH / 2 + i * step_size,
+                                   -SIDE_LENGTH / 2, +SIDE_LENGTH / 2};
+    }
+    // side 3
+    for (int i = 2 * points_per_side; i < 3 * points_per_side; i++)
+    {
+        points[i] = (struct Point){-SIDE_LENGTH / 2 + i * step_size,
+                                   -SIDE_LENGTH / 2, +SIDE_LENGTH / 2};
+    }
+    
     // return points;
 }
 
@@ -65,10 +83,10 @@ int main()
 
     SDL_Surface *surface = SDL_GetWindowSurface(window);
     // struct Point point = {0, 0, 0};
-    int number_of_points = 100;
+    int number_of_points = 120;
     struct Point points[number_of_points];
     initialize_cube(points, number_of_points);
-    draw_points_3d(surface, points);
+    draw_points_3d(surface, points, number_of_points);
 
     // size of the shape & color of shape = white
     // SDL_Rect rect = (SDL_Rect){40, 40, 30, 60};
