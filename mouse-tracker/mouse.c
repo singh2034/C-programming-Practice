2231ab3c13476cc40f6ee39bcf2e1288ff008c9c
singh2034 <singhaayush2034@gmail.com>
2026-03-15 21:27:21 +0530

    updated

--- mouse-tracker/mouse.c
+++ mouse-tracker/mouse.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
 int main (int argc, char* argv[]) 
 {
     if (argc != 2)
     {
-        printf("Usage: %s <event-file-path> \n" argv[0]);
+        printf("Usage: %s <event-file-path>\n", argv[0]);
         exit(0);
     }
-    printf("Mouse movements are intercepted here .... \n");
-
+    char* devicepath = argv[1];
+    printf("Mouse %s movements are intercepted here .... \n", devicepath);
+    int fd = open(devicepath, 0, O_RDONLY);
+    printf("File Descriptor: %d\n", fd);
 }
